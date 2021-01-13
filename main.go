8b742fcc071bbf4d7587c26d9d8d0fdb340107f8
David Orson <onlyorson@outlook.com>
2021-01-13 15:06:21 +0000

    get /todos

--- main.go
+++ main.go
@@ -1,10 +1,27 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
+
+type Todo struct {
+	Body string `json:"Body"`
+	Completed bool `json:"Completed"` 
+}
+
+type Todos []Todo
+
+func allTodos(w http.ResponseWriter, r *http.Request) {
+	todos := Todos{
+		Todo{Body:"Take out the trash", Completed:true },
+	}
+
+	fmt.Println("Endpoint Hit: All Todos Endpoint")
+	json.NewEncoder(w).Encode((todos))
+}
 
 func homePage(w http.ResponseWriter, r *http.Request)  {
 	fmt.Fprint(w, "Homepage Endpoint Hit")
@@ -12,6 +29,7 @@ func homePage(w http.ResponseWriter, r *http.Request)  {
 
 func handleRequests() {
 	http.HandleFunc("/", homePage)
+	http.HandleFunc("/todos", allTodos)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
