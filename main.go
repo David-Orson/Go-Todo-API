0ba94ae7ee5bb4e86203837461348cd4c57cd564
David Orson <onlyorson@outlook.com>
2021-01-13 14:55:18 +0000

    Home Enpoint Hit

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
-func main ()  {
-	fmt.Println("Hello World")
+func homePage(w http.ResponseWriter, r *http.Request)  {
+	fmt.Fprint(w, "Homepage Endpoint Hit")
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func main() {
+	handleRequests()
 }
