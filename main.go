a155440d2740d4c8b5fd8d1b131043eba8dfe6cf
David Orson <onlyorson@outlook.com>
2021-01-13 22:23:48 +0000

    CORS Access Control

--- main.go
+++ main.go
@@ -10,16 +10,21 @@ import (
 )
 
 type Todo struct {
-	Body string `json:"Body"`
-	Completed bool `json:"Completed"` 
+	Id int `json:"id"`
+	Body string `json:"body"`
+	Completed bool `json:"completed"` 
 }
 
 type Todos []Todo
 
 func allTodos(w http.ResponseWriter, r *http.Request) {
 	todos := Todos{
-		Todo{Body:"Take out the trash", Completed:true },
+		Todo{Id:1, Body:"Take out the trash", Completed:false },
+		Todo{Id: 2, Body: "Clean the dishes",	Completed: false },
+		Todo{Id: 3,	Body: "Walk the dog", Completed: false },
 	}
+
+	enableCors(&w)
 
 	fmt.Println("Endpoint Hit: All Todos Endpoint")
 	json.NewEncoder(w).Encode((todos))
@@ -34,8 +39,12 @@ func handleRequests() {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/", homePage)
-	router.HandleFunc("/todos", allTodos).Methods("GET")
-	log.Fatal(http.ListenAndServe(":8080", router))
+	router.HandleFunc("/api/todos", allTodos).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8081", router))
+}
+
+func enableCors(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
 }
 
 func main() {
